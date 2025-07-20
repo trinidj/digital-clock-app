c0bda9eab2f89190822b3569954bcf6804d0e020
trinidj <jm.trinidad199@gmail.com>
2025-07-19 23:40:41 -0400

    feat: Add function to update display

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ import { ClockManager } from "./helpers/ClockManager.js";
 
 const clockManager = new ClockManager();
 const dateDisplay = document.querySelector('.date-display');
+const hourDisplay = document.querySelector('.hours');
+const minuteDisplay = document.querySelector('.minutes');
+const secondDisplay = document.querySelector('.seconds');
 
-dateDisplay.innerHTML = clockManager.formattedDate();
+const updateDisplay = () => {
+  const [ hours, minutes, seconds ] = clockManager.formatTime();
+
+  dateDisplay.innerHTML = clockManager.formatDate();
+  hourDisplay.innerHTML = hours;
+  minuteDisplay.innerHTML = minutes;
+  secondDisplay.innerHTML = seconds;
+};
+
+updateDisplay();
+
+setInterval(() => {
+  clockManager.updateTime();
+  updateDisplay();
+}, 1000);
 
 document.addEventListener('DOMContentLoaded', () => {
   const clockButton = document.querySelector('.digital-clock');
