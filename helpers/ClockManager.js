608b1b1b02eb34c0e264a1b31985e50d84fe147d
trinidj <jm.trinidad199@gmail.com>
2025-07-19 23:39:38 -0400

    feat: Update clock in real time

--- helpers/ClockManager.js
+++ helpers/ClockManager.js
@@ -2,20 +2,33 @@ export class ClockManager {
   constructor() {
     // Date Variables
     this.date = new Date();
-    
-    this.currentTime = {
-      hours: this.date.getHours(),
-      minutes: this.date.getMinutes(),
-      seconds: this.date.getSeconds(),
-    };
+
+    this.hours = this.date.getHours();
+    this.minutes = this.date.getMinutes();
+    this.seconds = this.date.getSeconds();
   }
 
-  getDate = () => {
+  formatDate = () => {
     return this.date.toLocaleDateString('en-US', {
       weekday: 'long',
       year: 'numeric',
       month: 'long',
       day: 'numeric'
     });
+  }
+
+  formatTime = () => {
+    return [
+      this.hours.toString().padStart(2, '0'),
+      this.minutes.toString().padStart(2, '0'),
+      this.seconds.toString().padStart(2, '0'),
+    ]
+  }
+
+  updateTime = () => {
+    this.date = new Date();
+    this.hours = this.date.getHours();
+    this.minutes = this.date.getMinutes();
+    this.seconds = this.date.getSeconds();
   }
 }
