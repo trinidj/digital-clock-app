9d0ed45ac9148e0014c44bdf9a01652b15dfcb96
trinidj <jm.trinidad199@gmail.com>
2025-07-21 15:23:36 -0400

    feat: Add svg icons for play-pause button

---
+++ helpers/svgIcons.js
@@ -0,0 +1,13 @@
+export const svgIcons = {
+  play: /*html*/`
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play-icon lucide-play">
+      <polygon points="6 3 20 12 6 21 6 3"/>
+    </svg>
+  `,
+
+  pause: /*html*/`
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pause-icon lucide-pause"><rect x="14" y="4" width="4" height="16" rx="1"/>
+      <rect x="6" y="4" width="4" height="16" rx="1"/>
+    </svg>
+  `,
+}
